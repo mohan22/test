5ae818c11757e97086a33e6d4f1d42262a6339a4
mohan kumar <jakkula4@gmail.com>
2012-06-04 17:15:05 +0530

    trail

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Trail::Application.config.secret_token = '1024ec5ffedc73e1283a0c3c7131a7892b36b25c5b5a6b3bc5c09d2e0c4ecf4ff9a8bf7e203cec1499b4a8089e2c856a7efdd880b81db11b0914c8585c8e3962'
